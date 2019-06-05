9e9bfc44c5a663402a32dbb6735cbc56fc6238a4
Mert Kimyonsen <mert.kimyonsen@betek.com.tr>
2019-06-06 01:00:51 +0300

    Nhibernate SqlServerProvider feature added

---
+++ DevFramework.DataAccess/Concrete/NHibernate/Mappings/ProductMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using DevFramework.Entities.Concrete;
+
+namespace DevFramework.DataAccess.Concrete.NHibernate.Mappings
+{
+    public class ProductMap : ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Table(@"Products");
+            LazyLoad();
+            Id(x => x.ProductId).Column("ProductID");
+
+            Map(x => x.CategoryId).Column("CategoryID");
+            Map(x => x.ProductName).Column("ProductName");
+            Map(x => x.QuantityPerUnit).Column("QuantityPerUnit");
+            Map(x => x.ReorderLevel).Column("ReorderLevel");
+            Map(x => x.SupplierId).Column("SupplierID");
+            Map(x => x.UnitPrice).Column("UnitPrice");
+            Map(x => x.UnitsInStock).Column("UnitsInStock");
+            Map(x => x.UnitsOnOrder).Column("UnitsOnOrder");
+            Map(x => x.Discontinued).Column("Discontinued");
+        }
+    }
+}
---
+++ DevFramework.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.DataAccess.NHibernate;
+using DevFramework.Entities.Concrete;
+using DevFramework.DataAccess.Abstract;
+
+namespace DevFramework.DataAccess.Concrete.NHibernate
+{
+    public class NhProductDal : NhEntityRepositoryBase<Product>, IProductDal
+    {
+        public NhProductDal(NHibernateProvider nHibernateProvider) : base(nHibernateProvider)
+        {
+        }
+    }
+}
