a8682e8439a51910d88f3d7cc65cdf19d428c60f
Mert Kimyonsen <mert.kimyonsen@betek.com.tr>
2019-06-06 23:15:00 +0300

    Manager implementation

---
+++ DevFramework.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Entities.Concrete;
+
+namespace DevFramework.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+        Product GetProductByID(int id);
+        Product Add(Product entity);
+    }
+}
---
+++ DevFramework.Business/Concrete/Managers/IProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Business.Abstract;
+using DevFramework.Entities.Concrete;
+using DevFramework.DataAccess.Abstract;
+using DevFramework.DataAccess.Concrete.EntityFramework;
+
+namespace DevFramework.Business.Concrete.Managers
+{
+    public class IProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public IProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product entity)
+        {
+            return _productDal.Add(entity);
+        }
+
+        public Product GetProductByID(int id)
+        {
+            return _productDal.Get(x => x.ProductId == id);
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _productDal.GetList();
+        }
+    }
+}
