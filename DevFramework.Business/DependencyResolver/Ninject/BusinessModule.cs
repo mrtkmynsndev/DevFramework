a35758eb69e8b082366d1de9a7a3183fb0836b28
Mert Kimyonsen <mert.kimyonsen@betek.com.tr>
2019-06-08 01:06:40 +0300

    Dependenct Injection feature added

---
+++ DevFramework.Business/DependencyResolver/Ninject/BusinessModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Business.Abstract;
+using DevFramework.Business.Concrete.Managers;
+using DevFramework.DataAccess.Abstract;
+using DevFramework.DataAccess.Concrete.EntityFramework;
+using DevFramework.Core.DataAccess;
+using DevFramework.Core.DataAccess.EntityFramework;
+using Ninject.Modules;
+using DevFramework.Core.DataAccess.NHibernate;
+using DevFramework.DataAccess.Concrete.NHibernate.Provider;
+
+namespace DevFramework.Business.DependencyResolver.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+            Bind<ICategoryService>().To<CategoryManager>().InSingletonScope();
+            Bind<ICategoryDal>().To<EfCategoryDal>().InSingletonScope();
+
+            Bind(typeof(IQueryableRepository<>)).To(typeof(EfQueryableRepository<>));
+            Bind<DbContext>().To<NorthwindContext>();
+            Bind<NHibernateProvider>().To<SqlServerProvider>();
+        }
+    }
+}
---
+++ DevFramework.Business/DependencyResolver/Ninject/ValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Business.ValidationRules.FluentValidation;
+using DevFramework.Entities.Concrete;
+using FluentValidation;
+using Ninject.Modules;
+
+namespace DevFramework.Business.DependencyResolver.Ninject
+{
+    public class ValidationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Product>>().To<ProductValidator>().InSingletonScope();
+        }
+    }
+}
