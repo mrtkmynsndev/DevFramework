ebc80ed7f31b753bec86d97d8de711b243e9b099
Mert Kimyonsen <mert.kimyonsen@betek.com.tr>
2019-06-09 00:49:41 +0300

    Autofac dependency resolver feature added

---
+++ DevFramework.Business/DependencyResolver/Autofac/BusinessModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using DevFramework.Business.Abstract;
+using DevFramework.Business.Concrete.Managers;
+using DevFramework.Core.DataAccess;
+using DevFramework.Core.DataAccess.EntityFramework;
+using DevFramework.Core.DataAccess.NHibernate;
+using DevFramework.DataAccess.Abstract;
+using DevFramework.DataAccess.Concrete.EntityFramework;
+using DevFramework.DataAccess.Concrete.NHibernate.Provider;
+using Module = Autofac.Module;
+
+namespace DevFramework.Business.DependencyResolver.Autofac
+{
+    public class BusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //base.Load(builder);
+
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+
+            builder.RegisterType<DbContext>().As<NorthwindContext>();
+            builder.RegisterType<NHibernateProvider>().As<SqlServerProvider>();
+            builder.RegisterType(typeof(EfQueryableRepository<>)).As(typeof(IQueryableRepository<>));
+
+        }
+    }
+}
