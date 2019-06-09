dea645e633f361419e4084305612dcb8ca351e84
Mert Kimyonsen <mert.kimyonsen@betek.com.tr>
2019-06-09 23:49:18 +0300

    Log Implementation

---
+++ DevFramework.Core/Aspects/Postsharp/LogAspects/LogAspect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+using DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net;
+using System.Reflection;
+using DevFramework.Core.CrossCuttingConcerns.Logging;
+
+namespace DevFramework.Core.Aspects.Postsharp.LogAspects
+{
+    public class LogAspect : OnMethodBoundaryAspect
+    {
+        LoggerService _loggerService;
+        Type _loggerType;
+
+        public LogAspect(Type loggerType)
+        {
+            _loggerType = loggerType;
+        }
+
+        public override void RuntimeInitialize(MethodBase method)
+        {
+            if (!_loggerType.IsAssignableFrom(typeof(LoggerService)))
+                throw new Exception("Invalid Logger Type");
+
+            _loggerService = (LoggerService)Activator.CreateInstance(_loggerType);
+
+            base.RuntimeInitialize(method);
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            if (!_loggerService.IsInfoEnable)
+                return;
+
+            try
+            {
+
+                var logParameters = args.Method.GetParameters().Select((t, i) => new LogParameter()
+                {
+                    Name = t.Name,
+                    Type = t.ParameterType.Name,
+                    Value = args.Arguments.GetArgument(i)
+                }).ToList();
+
+                var logDetail = new LogDetail()
+                {
+                    FullName = args.Method.DeclaringType == null ? null : args.Method.DeclaringType.Name,
+                    MethodName = args.Method.Name,
+                    Parameters = logParameters
+                };
+
+                _loggerService.Info(logDetail);
+
+            }
+            catch
+            {
+                //ignored
+            }
+
+            base.OnEntry(args);
+        }
+    }
+}
---
+++ DevFramework.Core/CrossCuttingConcerns/Logging/Log4Net/LoggerService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    public class LoggerService
+    {
+        ILog _log;
+
+        public LoggerService(ILog log)
+        {
+            _log = log;
+        }
+
+        public bool IsInfoEnable => _log.IsInfoEnabled;
+        public bool IsDebugEnabled => _log.IsDebugEnabled;
+        public bool IsWarnEnabled => _log.IsWarnEnabled;
+        public bool IsFatalEnabled => _log.IsFatalEnabled;
+        public bool IsErrorEnabled => _log.IsFatalEnabled;
+
+        public void Info(object logMessage)
+        {
+            if (IsInfoEnable)
+                _log.Info(logMessage);
+        }
+
+        public void Debug(object logMessage)
+        {
+            if (IsDebugEnabled)
+                _log.Debug(logMessage);
+        }
+        public void Warn(object logMessage)
+        {
+            if (IsDebugEnabled)
+                _log.Warn(logMessage);
+        }
+        public void Fatal(object logMessage)
+        {
+            if (IsDebugEnabled)
+                _log.Fatal(logMessage);
+        }
+        public void Error(object logMessage)
+        {
+            if (IsDebugEnabled)
+                _log.Error(logMessage);
+        }
+    }
+}
